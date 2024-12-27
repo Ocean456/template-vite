697d9dcc7a22900e372aa720b13f391450400775
22827 <2282724674@qq.com>
2024-12-27 16:33:17 +0800

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const home = ref('home');
+</script>
+
+<template>
+    <div id="home">
+      {{home}}
+      <el-button>默认按钮</el-button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
